986addcda7abba29eb7fae7c3156ede69c2f8b09
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 11:26:41 +0700

    WIP: Initial tests shell

---
+++ tests/HtmlTaglineSiteConfigTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class HtmlTaglineSiteConfigTest extends SapphireTest {
+	public function testUpdateCMSFields() {
+		$this->markTestSkipped('TODO');
+	}
+
+	public function testPopulateDefaults() {
+		$this->markTestSkipped('TODO');
+	}
+
+}
