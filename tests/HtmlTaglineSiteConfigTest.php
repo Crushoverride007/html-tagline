fbee262174a42bf7f572c6269b4ee9162b3fb786
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-20 13:22:05 +0700

    ENHANCEMENT: Failing test, populateDefaults() not working as expected

--- tests/HtmlTaglineSiteConfigTest.php
+++ tests/HtmlTaglineSiteConfigTest.php
@@ -1,12 +1,32 @@
 <?php
 
-class HtmlTaglineSiteConfigTest extends SapphireTest {
-	public function testUpdateCMSFields() {
-		$this->markTestSkipped('TODO');
-	}
+class HtmlTaglineSiteConfigTest extends SapphireTest
+{
+    public function testUpdateCMSFields()
+    {
+        $siteConfig = new SiteConfig();
+        $fields = $siteConfig->getCMSFields();
+        $tab = $fields->findOrMakeTab('Root.Main');
+        $fields = $tab->fieldList();
+        $names = array();
+        foreach ($fields as $field) {
+            array_push($names, $field->getName());
+        }
+        error_log(print_r($names,1));
 
-	public function testPopulateDefaults() {
-		$this->markTestSkipped('TODO');
-	}
+        // Check that TagLine has been replaced by HTMLTagLine
+        $this->assertContains('HTMLTagLine', $names);
+        $this->assertNotContains('TagLine', $names);
+    }
 
+    public function testPopulateDefaults()
+    {
+        $siteConfig = new SiteConfig();
+        $siteConfig->populateDefaults();
+        $fields = $siteConfig->getCMSFields();
+        $tab = $fields->findOrMakeTab('Root.Main');
+        $fields = $tab->fieldList();
+        $value = $fields->fieldByName('HTMLTagLine')->value();
+        $this->assertEquals('<p>your tagline here</p>', $value);
+    }
 }
